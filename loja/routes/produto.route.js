0d8d7bc799d0cf95cfc8625708a3ad66eb49a76c
Italo Tavares <italotavares@ufg.br>
2018-10-30 15:24:14 -0300

    comentarios das rotas

--- loja/routes/produto.route.js
+++ loja/routes/produto.route.js
@@ -3,11 +3,15 @@ const router = express.Router();
 
 const produto_controller = require('../controllers/produto.controller');
 
+// retorna todos os registros do banco
 router.get('/registros', produto_controller.registros);
-router.get('/registro', produto_controller.registros);
+// detalha os dados do registro passado por ID
 router.get('/busca/:id', produto_controller.detalhar);
+// cria um produto através dos parametros passados no formato json
 router.post('/criar', produto_controller.criar);
+// atualiza os dados do produto através do ID
 router.put('/atualizar/:id', produto_controller.atualizar);
+// exclui o produto da ID informada
 router.delete('/excluir/:id', produto_controller.excluir);
 
 module.exports = router;
